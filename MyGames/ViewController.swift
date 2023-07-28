f5021d37a311a01393ac731b7aca7f9c5bff841c
SMKATO <moraes.kato@gmail.com>
2023-07-27 21:10:17 -0300

    Initial Commit

---
+++ MyGames/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyGames
+//
+//  Created by Kato on 27/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
